fe2c43bef4ae71ebf84151fe939785aa1bfba910
LengxiQwQ <lengxiowo@gmail.com>
2025-11-14 13:18:00 +0800

    Initialization

---
+++ src/carrental/ui/Main.java
@@ -0,0 +1,8 @@
+package carrental.ui;
+
+public class Main {  // 修改类名为大写开头
+    public static void main(String[] args) {  // 添加 public 访问修饰符（虽然不是必须的，但是标准做法）
+        System.out.println("Hello World!");
+        System.out.println("Car Rental System Started");
+    }
+}
--- src/carrental/ui/main.java
+++
@@ -1,9 +0,0 @@
-package carrental.ui;
-
-import java.net.SocketOption;
-
-public class main {
-    static void main(String[] args) {
-        System.out.print("Hello World!");
-    }
-}
